3275548b8d3bfca6fcf0b63e7a73f2ee15e06208
aenoncunanan <aenon_cunanan2@yahoo.com>
2016-02-17 15:42:34 +0800

    Initial commit

---
+++ src/ph/edu/dlsu/Main.java
@@ -0,0 +1,29 @@
+package ph.edu.dlsu;
+import java.io.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        int intOption = 0;
+
+        while(intOption!=8) {
+
+            String p = InputHelper.getInput("Input a boolean value for X : ");
+            String q = InputHelper.getInput("Input a boolean value for Y : ");
+
+            p = BinaryConverter.convertFirstBinaryInput(p);
+            q = BinaryConverter.convertSecondBinaryInput(q);
+
+            boolean X = Boolean.parseBoolean(p);
+            boolean Y = Boolean.parseBoolean(q);
+
+            String strOption = InputLogic.getOption();
+
+            intOption = Integer.parseInt(strOption);
+
+            SwicthLogic.switchOption(intOption, X, Y);
+
+        }
+    }
+}
